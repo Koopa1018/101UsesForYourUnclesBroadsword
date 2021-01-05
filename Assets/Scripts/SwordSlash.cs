2c0923d1f1de2ea5792c96a4b353a60995c3216b
Koopa1018 <caleblovestoytrains@gmail.com>
2021-01-05 03:12:24 -0700

    Got sword kinda working
    And graphically overhauled. Somehow I didn't end up committing the graphics? so I put those in, along with animations and a quick 'n' dirty sword animator.
    
    Player graphic has additionally been reduced to a box. If we decide to go full graphics, let me know so I can set that up.

--- Assets/Scripts/SwordSlash.cs
+++ Assets/Scripts/SwordSlash.cs
@@ -1,23 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 using Clouds.Platformer.Forces;
 
 public class SwordSlash : MonoBehaviour
 {
+	[Header("Inputs")]
+	[SerializeField] InputActionAsset inputMap;
+	[SerializeField] string actionMapName = "Gameplay";
+	[SerializeField] InputActionReference mouseAction;
+	[SerializeField] InputActionReference attackAction;
+
+	[Header("Fields")]
     public AcceptsForces forceAcceptor;
     public float swordLength = 1.5f;
     public Transform player;
     public float feetOffset = 0.75f;
     public LayerMask layerMask;
+
+	[Header("Animator")]
+	public Animator swordAnimator;
     
     bool attack = false;
-    
-    // Start is called before the first frame update
-    void Update()
-    {
-        attack = Input.GetButtonDown("Slash");
-    }
+
+	void Start () {
+		//Necessary new-input-system init[ialization].
+		inputMap.FindActionMap(actionMapName, true).Enable();
+
+		
+	}
 
     // Update is called once per frame
     void FixedUpdate()
@@ -25,12 +38,15 @@ public class SwordSlash : MonoBehaviour
         Vector3 offsetOrigin = player.position + Vector3.up * feetOffset;
         
         // mouse location relative to player
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(mouseAction.action.ReadValue<Vector2>());
         mousePosition -= offsetOrigin;
         Debug.DrawRay(offsetOrigin, mousePosition);
 
         // when click create Hitbox
-        if (attack){
+        if (attackAction.action.ReadValue<float>() > 0) {
+			//animate sword to attack
+			swordAnimator.SetTrigger("Swing");
+
             RaycastHit2D swordHit = Physics2D.Raycast(
                 offsetOrigin, // origin of the raycast
                 mousePosition.normalized, // direction of the raycast
